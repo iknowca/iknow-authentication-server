5b70c1e9b8a46b31f7f82ba3d758b1abe11fc50d
iknowca <iknowca@gmail.com>
2024-01-28 03:42:05 +0900

    INIT: 프로젝트 저장소 설정 [IAS-1]

---
+++ src/main/java/xyz/iknow/authenticaionserver/AuthenticaionserverApplication.java
@@ -0,0 +1,13 @@
+package xyz.iknow.authenticaionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticaionserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticaionserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/iknow/authenticaionserver/AuthenticaionserverApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.iknow.authenticaionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticaionserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
