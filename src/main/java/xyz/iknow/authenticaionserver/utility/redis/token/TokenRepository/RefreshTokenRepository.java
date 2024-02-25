9b07f56c6ff298e14c5332f06c3d5864f12df3ed
iknowca <iknowca@gmail.com>
2024-02-25 21:58:37 +0900

    ⏪ rollback: [IAS-14]

--- src/main/java/xyz/iknow/authenticaionserver/utility/redis/token/TokenRepository/RefreshTokenRepository.java
+++ src/main/java/xyz/iknow/authenticaionserver/utility/redis/token/TokenRepository/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package xyz.iknow.authenticaionserver.utility.redis.token.TokenRepository;
 
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import xyz.iknow.authenticaionserver.utility.redis.token.token.RefreshToken;
@@ -7,6 +8,7 @@ import xyz.iknow.authenticaionserver.utility.redis.token.token.RefreshToken;
 import java.util.Optional;
 
 @Repository
+@EnableRedisRepositories(basePackages = "xyz.iknow.authenticaionserver.utility.redis.token.TokenRepository", redisTemplateRef = "redisTemplate")
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long>{
     Optional<RefreshToken> findById(Long id);
 }
